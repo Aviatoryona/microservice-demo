eba976f30460383be04510388e881782983b7995
Yonathaniel Mbogho <yonathanielmbogho@systechafrica.com>
2026-05-26 15:03:12 +0300

    Test Jenkins Webhook
    Issue:
    - Test webhook by commit/push
    
    Fix:
    - Test webhook by commit/push

--- user-service/src/main/java/com/mcsdemo/userservice/controller/UserController.java
+++ user-service/src/main/java/com/mcsdemo/userservice/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -26,5 +27,10 @@ public class UserController {
     @GetMapping
     public List<User> getAll() {
         return service.getAll();
+    }
+
+    @GetMapping
+    public User getUserById(@RequestParam Long id) {
+        return service.getUserById(id);
     }
 }
--- user-service/src/main/java/com/mcsdemo/userservice/service/UserService.java
+++ user-service/src/main/java/com/mcsdemo/userservice/service/UserService.java
@@ -20,4 +20,8 @@ public class UserService {
     public List<User> getAll() {
         return repository.findAll();
     }
+
+    public User getUserById(Long id) {
+        return repository.findById(id).orElse(null);
+    }
 }
